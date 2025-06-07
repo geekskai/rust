2e86c4cba21dc1dfe171a6bb176133d60300cb7a
geekskai <geeks.kai@gmail.com>
2025-06-07 12:15:32 +0800

    Add incremental build artifacts for the guessing game project
    - Generated object files and query cache for the guessing game.
    - Created work products and lock files to manage incremental compilation.
    - Updated to Rust version 1.87.0 (17067e9ac 2025-05-09).

---
+++ projects/guessing_game/target/debug/deps/guessing_game-70d8bf085117586a.d
@@ -0,0 +1,5 @@
+/Users/gankai/Desktop/geekskai/2025/rust/projects/guessing_game/target/debug/deps/guessing_game-70d8bf085117586a: src/main.rs
+
+/Users/gankai/Desktop/geekskai/2025/rust/projects/guessing_game/target/debug/deps/guessing_game-70d8bf085117586a.d: src/main.rs
+
+src/main.rs:
---
+++ projects/guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/Users/gankai/Desktop/geekskai/2025/rust/projects/guessing_game/target/debug/guessing_game: /Users/gankai/Desktop/geekskai/2025/rust/projects/guessing_game/src/main.rs
