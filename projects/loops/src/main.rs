b181ef02953fa5dabab71a52e21d54ba3d4ca3e0
geekskai <geeks.kai@gmail.com>
2025-06-15 18:22:13 +0800

    Add incremental compilation artifacts for loops project
    - Created new work-products binary for the loops project.
    - Initialized lock file for incremental compilation state.

--- projects/loops/src/main.rs
+++ projects/loops/src/main.rs
@@ -12,22 +12,34 @@ fn main() {
     // };
     // println!("The result is: {}", result);
 
-    let mut count = 0;
-    'counting_up: loop {
-        println!("count = {}", count);
-        let mut remaining = 10;
+    // -------------------------------
 
-        loop {
-            println!("remaining = {}", remaining);
-            if remaining == 9 {
-                break; // Breaks the inner loop
-            }
-            if count == 2 {
-                break 'counting_up; // Breaks the outer loop
-            }
-            remaining -= 1;
-        }
-        count += 1;
+    // let mut count = 0;
+    // 'counting_up: loop {
+    //     println!("count = {}", count);
+    //     let mut remaining = 10;
+
+    //     loop {
+    //         println!("remaining = {}", remaining);
+    //         if remaining == 9 {
+    //             break; // Breaks the inner loop
+    //         }
+    //         if count == 2 {
+    //             break 'counting_up; // Breaks the outer loop
+    //         }
+    //         remaining -= 1;
+    //     }
+    //     count += 1;
+    // }
+    // println!("End count = {}", count);
+
+    // ----------------------------------
+
+    let mut number = 3;
+    while number != 0 {
+        println!("{}!", number);
+        number -= 1;
     }
-    println!("End count = {}", count);
+    println!("LIFTOFF!!!");
+    // This is a simple while loop that counts down from 3 to 1 and then prints "LIFTOFF!!!".
 }
