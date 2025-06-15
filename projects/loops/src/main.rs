2f35f45548ef6aafa21ff78f639d3a8253081e1d
geekskai <geeks.kai@gmail.com>
2025-06-15 18:30:36 +0800

    Add incremental compilation artifacts for loops project
    - Created new work-products.bin file containing compiled outputs.
    - Initialized s-h8bf9ob0en-1inu5ap.lock file for incremental compilation tracking.

--- projects/loops/src/main.rs
+++ projects/loops/src/main.rs
@@ -35,11 +35,41 @@ fn main() {
 
     // ----------------------------------
 
-    let mut number = 3;
-    while number != 0 {
-        println!("{}!", number);
-        number -= 1;
-    }
-    println!("LIFTOFF!!!");
+    // let mut number = 3;
+    // while number != 0 {
+    //     println!("{}!", number);
+    //     number -= 1;
+    // }
+    // println!("LIFTOFF!!!");
     // This is a simple while loop that counts down from 3 to 1 and then prints "LIFTOFF!!!".
+
+    // ----------------------------------
+    // let array = [10, 20, 30, 40, 50];
+    // for element in array {
+    //     println!("Element: {}", element);
+    // }
+
+    // -----------------------------------
+    // for number in (1..4).rev() {
+    //     println!("rev: {}!", number);
+    // }
+    // println!("LIFTOFF!!!");
+
+    //  相互转换摄氏与华氏温度。
+    // let celsius = 100.0;
+    // let fahrenheit = celsius * 9.0 / 5.0 + 32.0;
+    // println!("{}°C is {}°F", celsius, fahrenheit);
+    // let fahrenheit = 212.0;
+    // let celsius = (fahrenheit - 32.0) * 5.0 / 9.0;
+    // println!("{}°F is {}°C", fahrenheit, celsius);
+
+    // • 生成第 n 个斐波那契数。
+
+    let n = 10;
+    let mut fib = vec![0, 1];
+    for i in 2..n {
+        let next_fib = fib[i - 1] + fib[i - 2];
+        fib.push(next_fib);
+    }
+    println!("The {}th Fibonacci number is: {}", n, fib[n - 1]);
 }
