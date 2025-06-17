7af3dbe1fe793f859395c6eda1bf4e5d79baca9b
geekskai <geeks.kai@gmail.com>
2025-06-18 07:04:17 +0800

    Add work products and lock file for incremental build in reference project
    - Created a new binary work-products file containing build artifacts.
    - Added a lock file to track dependencies for the incremental build.

--- projects/reference/src/main.rs
+++ projects/reference/src/main.rs
@@ -4,14 +4,20 @@ fn main() {
     // let (s2, len) = calculate_length(s1); // s1 的所有权被转移到 calculate_length 函数
     // println!("The length of '{}' is {}.", s2, len);
 
-    let len = calculate_length1(&s1); // 传递 s1 的引用
-    println!("The length of '{}' is {}.", s1, len);
+    // let len = calculate_length1(&s1); // 传递 s1 的引用
+    // println!("The length of '{}' is {}.", s1, len);
+
+    change(&s1);
 }
 
-fn calculate_length1(s: &String) -> usize {
-    let len = s.len(); // 返回字符串的长度
-    len
+fn change(s: &String) {
+    s.push_str(", world!"); // 试图修改不可变引用
 }
+
+// fn calculate_length1(s: &String) -> usize {
+//     let len = s.len(); // 返回字符串的长度
+//     len
+// }
 
 // fn calculate_length(s: String) -> (String, usize) {
 //     let len = s.len(); // 计算字符串的长度
