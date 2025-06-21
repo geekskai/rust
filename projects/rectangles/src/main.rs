2b3403fe2276da0644f99dc9f5df2f87c7b982a6
geekskai <geeks.kai@gmail.com>
2025-06-21 11:59:35 +0800

    Add incremental build artifacts for rectangles project
    - Created new work-products.bin file containing build outputs.
    - Added lock file for incremental compilation tracking.

--- projects/rectangles/src/main.rs
+++ projects/rectangles/src/main.rs
@@ -1,17 +1,30 @@
-#[derive(Debug)]
+// #[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
     let scale = 3;
+
     let rect1 = Rectangle {
-        width: dbg!(30 * scale),
+        width: 30 * scale,
         height: 50,
     };
+    println!("rect1 is {}", rect1.area());
+    // rect1.area();
+    // let rect1 = Rectangle {
+    //     width: dbg!(30 * scale),
+    //     height: 50,
+    // };
 
-    dbg!(&rect1);
+    // dbg!(&rect1);
 
     // println!("The area of the rectangle is: {}", area(&rect1));
     // println!("rect1's area is: {rect1:#?}");
