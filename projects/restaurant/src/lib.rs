5508ff538d1118c7ff962a7a8b4a0de75796eae1
geekskai <geeks.kai@gmail.com>
2025-06-28 12:41:24 +0800

    Add incremental compilation artifacts for restaurant project
    - Generated work-products.bin containing compiled outputs.
    - Created lock file for incremental compilation state.

--- projects/restaurant/src/lib.rs
+++ projects/restaurant/src/lib.rs
@@ -10,10 +10,42 @@ mod front_of_house {
     }
 }
 
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String, // private field
+    }
+
+    impl Breakfast {
+        // Public constructor for Breakfast
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+}
+
+// pub fn eat_at_restaurant() {
+//     // Using the front_of_house module
+//     // absolute path
+//     crate::front_of_house::hosting::add_to_wait_list();
+//     // relative path
+//     front_of_house::hosting::add_to_wait_list();
+// }
+
 pub fn eat_at_restaurant() {
-    // Using the front_of_house module
-    // absolute path
-    crate::front_of_house::hosting::add_to_wait_list();
-    // relative path
-    front_of_house::hosting::add_to_wait_list();
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // Trying to access the private field (this will fail)
+    // println!("I'd like {} please", meal.seasonal_fruit);
+
+    // meal.seasonal_fruit = String::from("blueberries");
+    // println!(
+    //     "I'd like {} toast with {} fruit",
+    //     meal.toast, meal.seasonal_fruit
+    // );
 }
