06e35d140e779eb78b32ef69401c9fed69bc5e04
geekskai <geeks.kai@gmail.com>
2025-06-28 16:28:02 +0800

    Add incremental build artifacts for vector project
    - Generated work-products.bin containing compiled outputs and metadata.
    - Created s-h8pnvejm5c-1k6zmtc.lock file for incremental compilation tracking.

--- projects/vector/src/main.rs
+++ projects/vector/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let v = vec![1, 2, 3, 4, 5];
+    // let v = vec![1, 2, 3, 4, 5];
 
     // let third: &i32 = &v[2];
 
@@ -13,7 +13,38 @@ fn main() {
     // }
 
     // let not_exists = v.get(100);
-    let not_exists = &v[100]; // This will panic at runtime if uncommented
+    // let not_exists = &v[100]; // This will panic at runtime if uncommented
 
-    println!("The value of not_exists is {:?}", not_exists);
+    // println!("The value of not_exists is {:?}", not_exists);
+
+    // for i in &v {
+    //     println!("The value is {}", i);
+    // }
+
+    // let mut v = vec![1, 2, 3, 4, 5];
+
+    // for i in &mut v {
+    //     *i += 50;
+    //     println!("The value is {}", i);
+    // }
+
+    // let data = "hello world";      // 类型: &str
+    // let s = data.to_string();      // 类型: String
+
+    // s.push('!'); // 合法，String 可变
+    // data.push('!'); // 不合法，&str 不可变
+
+    let data = "hello world";
+
+    // let s = data.to_string();
+
+    println!("The string is: {}", data);
+
+    for c in "Зд".chars() {
+        println!("{c}");
+    }
+
+    for b in "Зд".bytes() {
+        println!("{b}");
+    }
 }
