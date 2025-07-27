e9dbabe1fd9938b2c93c2cebbc4994165a77a2f7
geekskai <geeks.kai@gmail.com>
2025-07-27 20:37:46 +0800

    Add incremental compilation artifacts for minigrep project

---
+++ projects/minigrep/target/debug/deps/minigrep-8c53ebfb69d548be.d
@@ -0,0 +1,5 @@
+/Users/gankai/Desktop/geekskai/2025/rust/projects/minigrep/target/debug/deps/minigrep-8c53ebfb69d548be: src/lib.rs
+
+/Users/gankai/Desktop/geekskai/2025/rust/projects/minigrep/target/debug/deps/minigrep-8c53ebfb69d548be.d: src/lib.rs
+
+src/lib.rs:
---
+++ projects/minigrep/target/debug/deps/minigrep-e58d5c464aff529e.d
@@ -0,0 +1,5 @@
+/Users/gankai/Desktop/geekskai/2025/rust/projects/minigrep/target/debug/deps/minigrep-e58d5c464aff529e: src/main.rs
+
+/Users/gankai/Desktop/geekskai/2025/rust/projects/minigrep/target/debug/deps/minigrep-e58d5c464aff529e.d: src/main.rs
+
+src/main.rs:
