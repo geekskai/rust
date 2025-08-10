22ebcf48f38d1e888541a46b98f3399825f6d3ae
geekskai <geeks.kai@gmail.com>
2025-08-10 22:14:26 +0800

    Initialize closures project with basic inventory structure and shirt color enumeration

---
+++ projects/closures/src/main.rs
@@ -0,0 +1,18 @@
+enum ShirtColor {
+    Red,
+    Blue,
+}
+
+struct Inventory {
+    shirts: Vec<ShirtColor>,
+}
+
+impl Inventory {
+    fn giveaway(&self, user_preference: Option<ShirtColor>) -> ShirtColor {
+        user_preference.unwrap_or_else(|| self.most_stocked())
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
