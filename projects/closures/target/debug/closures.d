1fd13c20b7d5501911ecf5616fa9cad31b190932
geekskai <geeks.kai@gmail.com>
2025-08-30 18:20:39 +0800

    Add functionality to inventory management with shirt color preference
    - Implemented `most_stocked` method to determine the most available shirt color.
    - Enhanced `giveaway` method to utilize user preferences and fallback to the most stocked shirt.
    - Updated `main` function to demonstrate the new inventory features with user preference examples.

---
+++ projects/closures/target/debug/closures.d
@@ -0,0 +1,1 @@
+/Users/gankai/Desktop/geekskai/2025/rust/projects/closures/target/debug/closures: /Users/gankai/Desktop/geekskai/2025/rust/projects/closures/src/main.rs
---
+++ projects/closures/target/debug/deps/closures-66cd77145fb61588.d
@@ -0,0 +1,5 @@
+/Users/gankai/Desktop/geekskai/2025/rust/projects/closures/target/debug/deps/closures-66cd77145fb61588: src/main.rs
+
+/Users/gankai/Desktop/geekskai/2025/rust/projects/closures/target/debug/deps/closures-66cd77145fb61588.d: src/main.rs
+
+src/main.rs:
